bb489c20d7e8bbe149877e7580074f57ac4f876d
artas-linux <artas.linux@gmail.com>
2025-10-06 04:03:56 +0000

    Initial commit: Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+/home/archbtw/.local/bin/mise activate fish | source # added by https://mise.run/fish
+source ~/.config/fish/user_variables.fish
+source ~/.config/fish/abbreviations.fish
+atuin init fish | source
+/home/archbtw/.local/bin/mise activate fish | source # added by https://mise.run/fish
